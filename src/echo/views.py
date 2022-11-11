58404ca2104780ad12c6bceee38d5465606758ea
Aleksey Korolev <koro6@yandex.ru>
2022-11-11 12:58:07 +0300

    fix isort error

--- src/echo/views.py
+++ src/echo/views.py
@@ -1,4 +1,5 @@
-from rest_framework import authentication, status
+from rest_framework import status
+from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
@@ -9,10 +10,7 @@ from echo.serializers import EchoSerializer
 
 class EchoView(ModelViewSet):
     queryset = Echo.objects.all().order_by("-created")
-    authentication_classes = [
-        authentication.TokenAuthentication,
-        authentication.BasicAuthentication,
-    ]
+    authentication_classes = BasicAuthentication
     permission_classes = [AllowAny]
     serializer_class = EchoSerializer
 
